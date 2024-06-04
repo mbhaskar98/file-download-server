4d944eb872dddc205dce68ffba34f14434d292bc
Bhaskar Mahajan <bmahajan@zscaler.com>
2024-06-04 12:40:22 +0530

    server code

--- main.go
+++ main.go
@@ -1,55 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"strings"``
+	"file_download_server/server"
+	"flag"
 )
 
+var (
+	host string
+	port string
+	dir  string
+)
+
+func init() {
+	flag.StringVar(&host, "host", "localhost", "Hostname to serve on")
+	flag.StringVar(&port, "port", "8080", "Port to serve on")
+	flag.StringVar(&dir, "dir", ".", "Directory containing the files to serve")
+}
+
 func main() {
-	if len(os.Args) != 4 {
-		log.Fatalf("Usage: %s %s %s %s", os.Args[0], "HOST", "PORT", "FILEPATH")
-	}
-	// host := "192.168.1.2"
-	host := os.Args[1]                  //"192.168.1.3"
-	port, _ := strconv.Atoi(os.Args[2]) // 8000
-	filePath := os.Args[3]
-	address := fmt.Sprintf("%s:%d", host, port)
-	fmt.Printf("Starting the server on: {%s}\n", address)
-	http.HandleFunc("/", sendFile(filePath))
-	err := http.ListenAndServe(address, nil)
-	if err != nil {
-		log.Fatal("Error while starting server:", err)
-	}
+	flag.Parse()
 
+	s := &server.Server{}
+	s.Start(host, port, dir)
 }
-
-func sendFile(path string) func(w http.ResponseWriter, r *http.Request) {
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		if len(path) == 0 {
-			path = "~/Desktop/10gb.bin"
-		}
-
-		tempArray := strings.Split(path, "/")
-		filename := tempArray[len(tempArray)-1]
-
-		fmt.Printf("Sending the file:%s to %s\n", filename, r.RemoteAddr)
-		w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote(filename))
-		w.Header().Set("Content-Type", "application/octet-stream")
-		http.ServeFile(w, r, path)
-	}
-}
-
-// func sendFile(w http.ResponseWriter, r *http.Request) {
-// 	filename := "10gb.bin"
-// 	temp := "/Users/bhaskarmahajan/Desktop/Tasks/HelperPrograms/file-download-server/"
-// 	filePath := fmt.Sprintf("%s/%s", temp, filename)
-// 	fmt.Printf("Sending the file:%s to %s\n", filename, r.RemoteAddr)
-// 	w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote(filename))
-// 	w.Header().Set("Content-Type", "application/octet-stream")
-// 	http.ServeFile(w, r, filePath)
-// }
